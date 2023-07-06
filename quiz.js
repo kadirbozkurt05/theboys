c089f310068e84cf14747fed98c865cce0cee446
Kadir Bozkurt <kadirbozkurt05@yandex.com>
2023-07-06 20:35:41 +0200

    almost done

--- quiz.js
+++ quiz.js
@@ -1,3 +1,7 @@
+/**
+ * Created this JS to give some logic to the embedded poll.
+ * As soon as page is loaded it gets the element and run the poll() function to make it usable.
+ */
 var modal;
 
 window.onload = function() {
@@ -5,6 +9,9 @@ window.onload = function() {
   poll();
 };
 
+/**
+ * Poll function makes the modal when visible if it is not visible, otherwise it makes it invisible.
+ */
 function poll() {
   if (modal.style.display === "none") {
     modal.style.display = "inline-block";
@@ -12,7 +19,9 @@ function poll() {
     closeModal();
   }
 }
-
+/**
+ * This function makes the modal invisible.
+ */
 function closeModal(){
     modal.style.display = "none";
 }
